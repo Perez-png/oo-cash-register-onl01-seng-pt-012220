5e6056fcfb663aff6c814e5996f9585cf20df20e
Xavier Perez <xperez007@gmail.com>
2020-02-18 20:40:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
+  attr_accessor :CashRegister, :CashRegister.new 
+  @cash_register = cash_register
+  @CashRegister = CashRegister.new
+  
+end
+
+def initialize.new(CashRegister).to eq(0)
+  cash_register = 0 
+  cash_register_with_discount = (20)
+  
+  
+  
+end
+
+
+
+
+
 
